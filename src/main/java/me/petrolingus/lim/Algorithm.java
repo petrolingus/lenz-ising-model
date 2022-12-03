6a591613ed835692afbd2b56a7d16c6bae496b72
petrolingus <petrolingus@gmail.com>
2022-12-03 11:20:47 +0300

    fix

--- src/main/java/me/petrolingus/lim/Algorithm.java
+++ src/main/java/me/petrolingus/lim/Algorithm.java
@@ -7,30 +7,11 @@ import static me.petrolingus.lim.Configuration.*;
 
 public class Algorithm {
 
-    private double time = 0;
-
     private int[][] matrix = new int[N][N];
-
+    private int time;
     private double energy;
 
     public void initialize() {
-
-        matrix = new int[N][N];
-
-        // Generate spins
-        generate();
-
-        energy = calculateEnergy();
-
-        // Check generated spins
-        testCorrectSpinGeneration();
-
-        System.out.println("Initialize done");
-        System.out.println("Init energy is equal to " + energy);
-    }
-
-    public void generate() {
-        matrix = new int[N][N];
 
         // Generate spins
         for (int i = 0; i < N * N / 2; i++) {
@@ -50,6 +31,15 @@ public class Algorithm {
                 }
             }
         }
+
+        time = 0;
+        energy = calculateEnergy();
+
+        // Check generated spins
+        testCorrectSpinGeneration();
+
+        System.out.println("Initialize done");
+        System.out.println("Init energy is equal to " + energy);
     }
 
     public void step() {
@@ -66,7 +56,7 @@ public class Algorithm {
                 double delta1 = deltaEnergy(y1, x1);
                 matrix[y1][x1] = s0;
                 double delta = 2 * (delta0 + delta1);
-                double gibbs = Math.exp(-delta / 0.5);
+                double gibbs = Math.exp(-delta / TEMPERATURE);
                 if (delta <= 0 || Math.random() < gibbs) {
 
                 } else {
@@ -74,15 +64,39 @@ public class Algorithm {
                     matrix[y1][x1] = s1;
                 }
             }
-            time += 1.0 / (N * N);
         }
+        time++;
     }
 
     public double getTime() {
         return time;
     }
 
-    public void testCorrectSpinGeneration() {
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    private double calculateEnergy() {
+        double energy = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int left = (j == 0) ? matrix[i][N - 1] : matrix[i][j - 1];
+                int top = (i == 0) ? matrix[N - 1][j] : matrix[i - 1][j];
+                energy += matrix[i][j] * (left + top);
+            }
+        }
+        return -J * energy;
+    }
+
+    private double deltaEnergy(int i, int j) {
+        int left = (j == 0) ? matrix[i][N - 1] : matrix[i][j - 1];
+        int right = (j == N - 1) ? matrix[i][0] : matrix[i][j + 1];
+        int top = (i == 0) ? matrix[N - 1][j] : matrix[i - 1][j];
+        int bottom = (i == N - 1) ? matrix[0][j] : matrix[i + 1][j];
+        return J * matrix[i][j] * (left + right + top + bottom);
+    }
+
+    private void testCorrectSpinGeneration() {
 
         int sum = 0;
         for (int i = 0; i < N; i++) {
@@ -93,87 +107,6 @@ public class Algorithm {
 
         if (sum != 0) {
             System.err.println("Not corrected fill matrix!");
-        }
-    }
-
-    public int[][] getMatrix() {
-        return matrix;
-    }
-
-    public double calculateEnergy() {
-
-        double energy = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                int left = (j == 0) ? matrix[i][N - 1] : matrix[i][j - 1];
-//                int right = (j == N - 1) ? matrix[i][0] : matrix[i][j + 1];
-                int top = (i == 0) ? matrix[N - 1][j] : matrix[i - 1][j];
-//                int bottom = (i == N - 1) ? matrix[0][j] : matrix[i + 1][j];
-                energy += matrix[i][j] * (left + top);
-            }
-        }
-
-        return -J * energy;
-    }
-
-    public double deltaEnergy(int i, int j) {
-        int left = (j == 0) ? matrix[i][N - 1] : matrix[i][j - 1];
-        int right = (j == N - 1) ? matrix[i][0] : matrix[i][j + 1];
-        int top = (i == 0) ? matrix[N - 1][j] : matrix[i - 1][j];
-        int bottom = (i == N - 1) ? matrix[0][j] : matrix[i + 1][j];
-        return J * matrix[i][j] * (left + right + top + bottom);
-    }
-
-    public void test() {
-
-        System.out.println("Start test");
-
-        for (int i = 0; i < 100_000; i++) {
-
-            generate();
-
-            double energy0 = calculateEnergy();
-
-            int x0;
-            int y0;
-            int x1;
-            int y1;
-            int s0;
-            int s1;
-
-            while (true) {
-                x0 = ThreadLocalRandom.current().nextInt(N);
-                y0 = ThreadLocalRandom.current().nextInt(N);
-                x1 = ThreadLocalRandom.current().nextInt(N);
-                y1 = ThreadLocalRandom.current().nextInt(N);
-                s0 = matrix[y0][x0];
-                s1 = matrix[y1][x1];
-                if (s0 != s1) {
-                    break;
-                }
-            }
-
-            matrix[y0][x0] = s1;
-            matrix[y1][x1] = s0;
-            double energy1 = calculateEnergy();
-            double diff0 = energy1 - energy0;
-
-            // Restore state
-            matrix[y0][x0] = s0;
-            matrix[y1][x1] = s1;
-
-            double delta0 = deltaEnergy(y0, x0);
-            matrix[y0][x0] = s1;
-            double delta1 = deltaEnergy(y1, x1);
-            matrix[y1][x1] = s0;
-            double delta = 2 * (delta0 + delta1);
-
-            if (delta != diff0) {
-                System.out.println("Test #" + i + " failed!");
-                System.out.println(new Point(x0, y0) + " " + new Point(x1, y1));
-                System.out.println("Diff0 = " + diff0);
-                System.out.println("delta = " + delta);
-            }
         }
     }
 }
--- src/main/java/me/petrolingus/lim/Configuration.java
+++ src/main/java/me/petrolingus/lim/Configuration.java
@@ -2,8 +2,10 @@ package me.petrolingus.lim;
 
 public interface Configuration {
 
-    int N = 512; // 20, 50, 100, 200, 500
+    int N = 800; // 20, 50, 100, 200, 500
 
     double J = 1;
+
+    double TEMPERATURE = 0.5;
 
 }
